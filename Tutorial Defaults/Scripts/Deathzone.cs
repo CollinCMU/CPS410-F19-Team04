7f352b8f32f1f23a2831742a638bdfc96211d27b
CollinCMU <vanbr1c@cmich.edu>
2019-11-18 14:58:50 -0500

    Test

---
+++ Tutorial Defaults/Scripts/Deathzone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathzone : MonoBehaviour
+{
+
+    public Transform spawn;
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        wait(5);
+        player.transform.position = spawn.position;
+    }
+
+    IEnumerator wait(int time)
+    {
+        yield return new WaitForSecondsRealtime(time);
+    }
+
+}
---
+++ Tutorial Defaults/Scripts/ExitLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLevel : MonoBehaviour
+{
+    bool activate = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            activate = true;
+        }
+        else
+        {
+            activate = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D player)
+    {
+        if (activate == true)
+            SceneManager.LoadScene("Level Select");
+    }
+}
