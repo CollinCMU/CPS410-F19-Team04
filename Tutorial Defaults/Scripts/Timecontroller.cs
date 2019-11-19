aebf0b9dc0aeb07cb56fc17331b9f3df05bb28d6
CollinCMU <vanbr1c@cmich.edu>
2019-11-18 21:48:34 -0500

    Main Menu redesign
    Co-Authored-By: blanc1tl <blanc1tl@users.noreply.github.com>

---
+++ Tutorial Defaults/Scripts/Timecontroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timecontroller : MonoBehaviour
+{
+    private bool paused = false;
+
+    #region Monobehaviour API
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+                Time.timeScale = 1;
+            else
+                Time.timeScale = 0;
+            paused = !paused;
+        }
+    }
+
+    #endregion
+}
